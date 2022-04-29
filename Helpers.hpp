6e77885e5ab7b825409fbeb694f91d24f41ab3a6
LemonHaze <guizhang@wulinshu.com>
2022-04-30 00:01:09 +0100

    Moved helper funcs to `Helpers.hpp`

---
+++ Helpers.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <filesystem>
+#include <fstream>
+#include <istream>
+#include <ostream>
+#include <iterator>
+#include <sstream>
+#include <assert.h>
+
+#define FOURST(x)		 std::string(reinterpret_cast<char*>(&x), 4)
+#define FOURCC(a,b,c,d)	 (signed int)((char)(a & 0xFF) << 24 | (char)(b & 0xFF) << 16 | (char)(c & 0xFF) << 8 | (char)(d & 0xFF))
+#define FLIP(x)			 (signed int)(( x >> 24 ) | (( x << 8) & 0x00ff0000 )| ((x >> 8) & 0x0000ff00) | ( x << 24))
+
+std::vector <std::string> FindFilesOfExtension(std::string searchDir, bool use_excludes = true) {
+	std::vector <std::string> res;
+	for (auto& path : std::filesystem::recursive_directory_iterator(searchDir)) {
+		bool bSkip = false;
+		if (use_excludes) {
+			for (auto& ext : exclude_extensions)
+				if (ext == path.path().extension().string())
+					bSkip = true;
+		}
+
+		// use bSkip if we're using the exclusion list or always save the path if not
+		if (use_excludes ? bSkip : true)
+		{
+			res.push_back(path.path().string());
+			(res.size() % 16 ? res.shrink_to_fit() : (void)0);
+		}
+	}
+	return res;
+}
+std::string GetFilename(std::string fullPath, bool with_extension = true) {
+	const size_t last_slash_idx = fullPath.find_last_of("\\/");
+	if (std::string::npos != last_slash_idx) {
+		fullPath.erase(0, last_slash_idx + 1);
+	}
+	if (!with_extension) {
+		const size_t period_idx = fullPath.rfind('.');
+		if (std::string::npos != period_idx)
+			fullPath.erase(period_idx);
+	}
+	return fullPath;
+}
+
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+	size_t start_pos = str.find(from);
+	if (start_pos == std::string::npos)
+		return false;
+	str.replace(start_pos, from.length(), to);
+	return true;
+}
--- pvr_xtractor.cpp
+++ pvr_xtractor.cpp
@@ -1,43 +1,7 @@
 /*
-		(S)ection (X)tractor
-
-		Aims to extract "sections" from a directory of files while respecting the original file structure. 
-		
-		A "section" is defined as anything which resembles the following structure:-
-		*---------------------------------------------------------------------------------------*
-		| Offset		|	Size	|	Value													|
-		*---------------------------------------------------------------------------------------*
-		| 0x0			|	 4		|	FOURCC Identifier (ie. 'PVRT' for PowerVR textures)		|
-		| 0x4			|	 4		|	Size of section in bytes								|
-		*---------------------------------------------------------------------------------------*
-		
-		Files are written to the input/current directory, or optionally to another directory with the third argument.
-
-		Example usage:-
-
-			* sx.exe "/path/to/files"
-			
-			* sx.exe "/path/to/files" "/path/to/output"
+		(S)ection (X)tractor	- LemonHaze - 2022
 */
-
-
-#define FOURST(x)		 std::string(reinterpret_cast<char*>(&x), 4)
-#define FOURCC(a,b,c,d)	 (signed int)((char)(a & 0xFF) << 24 | (char)(b & 0xFF) << 16 | (char)(c & 0xFF) << 8 | (char)(d & 0xFF))
-#define FLIP(x)			 (signed int)(( x >> 24 ) | (( x << 8) & 0x00ff0000 )| ((x >> 8) & 0x0000ff00) | ( x << 24))
-
-#include <Windows.h>
-#include <iostream>
-#include <vector>
-#include <string>
-#include <stdlib.h>
-#include <stdio.h>
-#include <filesystem>
-#include <fstream>
-#include <istream>
-#include <ostream>
-#include <iterator>
-#include <sstream>
-#include <assert.h>
+#include "Helpers.hpp"
 
 // Files extensions to explicitly exclude when processing files for sections
 std::vector <std::string> exclude_extensions = {
@@ -87,46 +51,6 @@ protected:
 	std::ifstream& stream;
 };
 
-std::vector <std::string> FindFilesOfExtension(std::string searchDir, bool use_excludes = true) {
-	std::vector <std::string> res;
-	for (auto& path : std::filesystem::recursive_directory_iterator(searchDir)) {
-		bool bSkip = false;
-		if (use_excludes) {
-			for (auto& ext : exclude_extensions)
-				if (ext == path.path().extension().string())
-					bSkip = true;
-		}
-
-		// use bSkip if we're using the exclusion list or always save the path if not
-		if (use_excludes ? bSkip : true)
-		{
-			res.push_back(path.path().string());
-			(res.size() % 16 ? res.shrink_to_fit() : (void)0);
-		}
-	}
-	return res;
-}
-std::string GetFilename(std::string fullPath, bool with_extension = true) {
-	const size_t last_slash_idx = fullPath.find_last_of("\\/");
-	if (std::string::npos != last_slash_idx) {
-		fullPath.erase(0, last_slash_idx + 1);
-	}
-	if (!with_extension) {
-		const size_t period_idx = fullPath.rfind('.');
-		if (std::string::npos != period_idx)
-			fullPath.erase(period_idx);
-	}
-	return fullPath;
-}
-
-bool replace(std::string& str, const std::string& from, const std::string& to) {
-	size_t start_pos = str.find(from);
-	if (start_pos == std::string::npos)
-		return false;
-	str.replace(start_pos, from.length(), to);
-	return true;
-}
-
 int main(int argc, char ** argp)
 {
 	bool bVerbose = true;
@@ -156,11 +80,11 @@ int main(int argc, char ** argp)
 					num_file++;
 
 					std::string filename = GetFilename(file, false).append("_").append(std::to_string(num_file)).append(tmpSection.getExtension());
+					
 					std::string path = std::filesystem::path(output_path).parent_path().string().append("\\" + filename).c_str();
-					std::ofstream output_file(path, std::ios::binary);
-
 					std::filesystem::create_directories(std::filesystem::path(output_path).parent_path());
-
+					
+					std::ofstream output_file(path, std::ios::binary);
 					if (output_file.good()) {
 						printf("Writing to %s..\n", path.c_str());
 						output_file.write(reinterpret_cast<char*>(&tmpSection.data.data()[0]), tmpSection.size);
